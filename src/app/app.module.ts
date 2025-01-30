7466f828f3018718bb74f4737408bc2f77af1d26
endiba360 <enrique.diaz@alumnos.udg.mx>
2025-01-30 13:24:12 -0600

    feat(main-footer): Create footer component and basic logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TaskListComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
